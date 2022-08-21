4efa3225a59026ca472cb07cbdda07e1552dd194
Jevin Sweval <jevinsweval@gmail.com>
2022-08-21 10:18:12 -0400

    flesh out args

--- dylibify/tool.py
+++ dylibify/tool.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+from pathlib import Path
 
 
 def real_main(args):
@@ -9,6 +10,16 @@ def real_main(args):
 
 def get_arg_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(description="dylibify")
+    parser.add_argument("-i", "--in", type=Path, help="Input Mach-O executable")
+    parser.add_argument("-o", "--out", type=Path, help="Output Mach-O dylib")
+    parser.add_argument(
+        "-d",
+        "--dylib-path",
+        help="Path for LC_ID_DYLIB command. e.g. @executable_path/Frameworks/libfoo.dylib",
+    )
+    platform_args = parser.add_mutually_exclusive_group()
+    platform_args.add_argument("-I", "--ios", action="store_true", help="Patch platform to iOS")
+    platform_args.add_argument("-M", "--macos", action="store_true", help="Patch platform to macOS")
     return parser
 
 
